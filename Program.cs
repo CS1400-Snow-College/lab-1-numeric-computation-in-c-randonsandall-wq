1bcfaedf062ba516aac3f29ea36029d2e40d8a15
unknown <randonsandall@students.snow.edu>
2026-01-21 22:39:37 -0700

    finished step: 4

--- Program.cs
+++ Program.cs
@@ -1,22 +1,5 @@
-﻿WorkWithIntegers();
-
-void WorkWithIntegers()
-{
-    int a = 18;
-    int b = 6;
-    int c = a + b;
-    Console.WriteLine(c);
-
-
-    // subtraction
-    c = a - b;
-    Console.WriteLine(c);
-
-    // multiplication
-    c = a * b;
-    Console.WriteLine(c);
-
-    // division
-    c = a / b;
-    Console.WriteLine(c);
-}
+﻿int a = 5;
+int b = 4;
+int c = 2;
+int d = a + b * c;
+Console.WriteLine(d);
