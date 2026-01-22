ae95e56f40244bc80ed703b89ce6340a11ffaaba
unknown <randonsandall@students.snow.edu>
2026-01-21 22:40:56 -0700

    finished step: 5

--- Program.cs
+++ Program.cs
@@ -1,12 +1,5 @@
-﻿int a = 5;
-int b = 4;
-int c = 2;
-int d = a + b * c;
-Console.WriteLine(d);
-
-d = (a + b) * c;
-Console.WriteLine(d);
-
-d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
-Console.WriteLine(d);
-
+﻿int e = 7;
+int f = 4;
+int g = 3;
+int h = (e + f) / g;
+Console.WriteLine(h);
