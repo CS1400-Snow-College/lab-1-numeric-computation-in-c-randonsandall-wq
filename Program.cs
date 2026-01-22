0b4114c6420c900dc05180897c4a47dad03da230
unknown <randonsandall@students.snow.edu>
2026-01-21 22:42:42 -0700

    finished step: 8

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-﻿double a = 5;
-double b = 4;
-double c = 2;
+﻿double a = 19;
+double b = 23;
+double c = 8;
 double d = (a + b) / c;
 Console.WriteLine(d);
+
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
